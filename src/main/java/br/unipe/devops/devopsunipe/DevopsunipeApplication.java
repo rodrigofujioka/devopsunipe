ace75d59c0df885af6167cc7e6f6acd12e5ec670
rfujioka <rodrigofujioka@gmail.com>
2025-11-29 09:02:57 -0300

    Commit inicial da aula

---
+++ src/main/java/br/unipe/devops/devopsunipe/DevopsunipeApplication.java
@@ -0,0 +1,13 @@
+package br.unipe.devops.devopsunipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsunipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsunipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/unipe/devops/devopsunipe/DevopsunipeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.unipe.devops.devopsunipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsunipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
